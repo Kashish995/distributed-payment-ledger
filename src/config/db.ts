672228d0b226104e28c5316ba6cbf7ac17736725
Kashish <kashishbehera9@gmail.com>
2026-07-21 13:29:43 +0530

    Prepare application for cloud deployment

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,10 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+export const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
